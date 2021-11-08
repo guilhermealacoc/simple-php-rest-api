de8c304385a2a86bbf3e5f059a9dbcf6237308ca
Guilherme de Alacoc Aquino <guilhermealacoc@gmail.com>
2021-11-07 21:09:54 -0300

    feat: teste

---
+++ config-example.php
@@ -0,0 +1,8 @@
+<?php
+
+    
+    const DBDRIVE = 'DBDRIVE';
+    const DBHOST = 'DBHOST';
+    const DBNAME = 'DBNAME';
+    const DBUSER = 'DBUSER';
+    const DBPASS = 'DBPASS';
--- config.php
+++ config.php
@@ -6,3 +6,5 @@
     const DBNAME = 'api';
     const DBUSER = 'php-rest';
     const DBPASS = 'password';
+
+    $db = new PDO(DBDRIVE.': host='.DBHOST.'; dbname='.DBNAME, DBUSER, DBPASS);
